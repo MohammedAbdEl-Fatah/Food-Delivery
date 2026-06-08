5910f2f426ac90d196c6f7b34f5358b11f2b2ff4
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-06-08 16:56:40 +0300

    repository of notification

---
+++ lib/features/notification/domain/repository/notification_repository.dart
@@ -0,0 +1,8 @@
+import 'package:food_delivery/features/notification/domain/entity/notification_entity.dart';
+
+abstract class NotificationRepository {
+  Stream<NotificationEntity> get foregroundNotifications;
+  Stream<NotificationEntity> get notificationTap;
+  Future<NotificationEntity> getInitialNotification();
+  Future<void> requestPermisson();
+}
