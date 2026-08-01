d74f2e402ea9ae57d29f3391f94eae6bbd83c117
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-08-01 20:02:54 +0300

    empty favorite widget

--- lib/features/favorite/presentation/view/favorite_screen.dart
+++ lib/features/favorite/presentation/view/favorite_screen.dart
@@ -1,14 +1,18 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:food_delivery/core/Colors/color_manager.dart';
+import 'package:food_delivery/core/style/app_text_style.dart';
+
+import '../../../layout/cubit/layout_cubit.dart';
+import '../widget/empty_favorite_widget.dart';
 
 class FavoriteScreen extends StatelessWidget {
   const FavoriteScreen({super.key});
-
+  //TODO give name prduct and image and price and add to cart button
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Favorite Screen'),
-      ),
-    );
+    return const Scaffold(body: EmptyFavoriteWidget());
   }
 }
---
+++ lib/features/favorite/presentation/widget/empty_favorite_widget.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/Colors/color_manager.dart';
+import '../../../../core/style/app_text_style.dart';
+import '../../../layout/cubit/layout_cubit.dart';
+
+class EmptyFavoriteWidget extends StatelessWidget {
+  const EmptyFavoriteWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.2,
+          ),
+          child: Icon(
+            Icons.no_food_outlined,
+            size: MediaQuery.of(context).size.width * 0.55,
+            color: ColorManager.primary,
+          ),
+        ),
+        Text(
+          'No Favorite Yet',
+          style: TextStyle(
+            fontSize: MediaQuery.of(context).size.width * 0.08,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+        Text(
+          'You have not added any favorite items yet.',
+          style: TextStyle(
+            fontSize: MediaQuery.of(context).size.width * 0.04,
+            color: ColorManager.grey,
+          ),
+        ),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+        ElevatedButton(
+          onPressed: () {
+            context.read<LayoutCubit>().changeIndex(0);
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: ColorManager.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          child: Text(
+            'Go to Home',
+            style: AppTextStyle.header6.copyWith(color: ColorManager.white),
+          ),
+        ),
+      ],
+    );
+  }
+}
